904ac6e2786df8fa918ce3861a59a362040b16fe
NoeGR <121056969+NoeGR@users.noreply.github.com>
2023-05-24 11:55:17 +0200

    Update DivisorFrecuencia.vhd

--- Codigo/DivisorFrecuencia.vhd
+++ Codigo/DivisorFrecuencia.vhd
@@ -1,6 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
+--use IEEE.STD_LOGIC_ARITH.ALL;
 entity DivisorFrecuencia is
 port (
 	clk100mhz: 	in STD_LOGIC;
@@ -10,14 +10,14 @@ end DivisorFrecuencia;
 
 architecture rtl of DivisorFrecuencia is
 	constant max_count: INTEGER := 100000000;
-	signal count: INTEGER range 0 to max_count;
+	signal count: INTEGER range 0 to max_count - 1;
 	signal clk_state: STD_LOGIC := '0';
 	
 begin
 	gen_clock: process(clk100mhz, clk_state, count)
 	begin
 		if clk100mhz'event and clk100mhz='1' then
-			if count < max_count then 
+			if count < max_count-1 then 
 				count <= count+1;
 			else
 				clk_state <= not clk_state;
